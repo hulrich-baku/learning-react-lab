60d31c3efb37b8a322a1864d848d5859e4f17f05
Hulrich Baku <hulrichbaku6@gmail.com>
2026-04-05 13:14:34 +0100

    chore(03-cine-fetch-engine): initial setup with Vite, TailwindCSS and security config

---
+++ 03-cine-fetch-engine/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-lg text-slate-500 font-bold text-center py-4">
+        CINE FETCH ENGINE
+      </h1>
+    </>
+  );
+}
+
+export default App;
