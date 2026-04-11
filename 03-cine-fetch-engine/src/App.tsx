d49ee952471a46ae0414af590d2fa23def65575d
Hulrich Baku <hulrichbaku6@gmail.com>
2026-04-11 22:10:25 +0100

    feat(03-cine-fetch-engine): implementation des pages (suivant)

--- 03-cine-fetch-engine/src/App.tsx
+++ 03-cine-fetch-engine/src/App.tsx
@@ -17,16 +17,32 @@ function App() {
     return () => clearTimeout(timer);
   }, [serachTerm]);
 
-  const { movies, loading, error } = useMovies(debouncedTerm);
-  const {toggleTheme, theme} = useTheme();
+  const { movies, loading, error, setPage, hasMore } = useMovies(debouncedTerm);
+  const { toggleTheme, theme } = useTheme();
 
   return (
     <main className="p-8 bg-slate-100 dark:bg-black min-h-screen transition-colors duration-500">
       {/* L'En-tête */}
-      <Header value={serachTerm} onChange={setSearchTerm} theme={theme} toggleTheme={toggleTheme} />
+      <Header
+        value={serachTerm}
+        onChange={setSearchTerm}
+        theme={theme}
+        toggleTheme={toggleTheme}
+      />
 
       {/* Grid qui gère l'affichage des films */}
       <MovieGrid movies={movies} loading={loading} error={error} />
+
+      {/* Bouton de page suivante */}
+      {hasMore && !loading && movies.length > 0 && (
+        <div className="flex justify-center mt-10">
+          <button 
+            onClick={() => setPage((prev) => prev + 1)} 
+            className="w-full py-2 sm:w-1/3 md:w-1/4 text-slate-100 bg-slate-800 dark:bg-slate-900 rounded-full hover:bg-slate-700 dark:hover:bg-slate-800 border border-black dark:border-slate-300 shadow-md shadow-black/90 dark:shadow-slate-600 transition-colors duration-500">
+            Charger plus de films
+          </button>
+        </div>
+      )}
     </main>
   );
 }
--- 03-cine-fetch-engine/src/hooks/useMovies.ts
+++ 03-cine-fetch-engine/src/hooks/useMovies.ts
@@ -7,6 +7,15 @@ export const useMovies = (searchQuery: string = "") => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+
+  // Reset quand la recherche change
+  useEffect(() => {
+    setMovies([]);
+    setPage(1);
+    setHasMore(true);
+  }, [searchQuery]);
 
   // 2. La fonction de récupération
   const fetchMovies = async () => {
@@ -15,13 +24,24 @@ export const useMovies = (searchQuery: string = "") => {
       setError(null);
 
       if (searchQuery.trim().length > 0) {
-        const data = await microService.searchMovies(searchQuery);
-        setMovies(data.results);
-      } else {
-        const data = await microService.getPopularMovies();
-        setMovies(data.results);
-      }
+        const data = await microService.searchMovies(searchQuery, page);
 
+        // on ajoute les nouveaux resultats si la page change
+        setMovies((prev) =>
+          page === 1 ? data.results : [...prev, ...data.results],
+        );
+
+        // vérification des pages restantes
+        setHasMore(page < data.total_pages);
+      } else {
+        const data = await microService.getPopularMovies(page);
+
+        setMovies((prev) =>
+          page === 1 ? data.results : [...prev, ...data.results],
+        );
+
+        setHasMore(page < data.total_pages);
+      }
     } catch (error) {
       setError("Impossible de charger les films. Vérifiez votre connexion.");
       console.log(error);
@@ -32,7 +52,7 @@ export const useMovies = (searchQuery: string = "") => {
 
   useEffect(() => {
     fetchMovies();
-  }, [searchQuery]);
+  }, [searchQuery, page]); // déclenche si la page ou la recherche change
 
-  return { movies, loading, error, refresh: fetchMovies };
+  return { movies, loading, error, hasMore, setPage, refresh: fetchMovies };
 };
